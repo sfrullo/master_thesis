331c56ac8c929bb5cea5096e8a5c026f05e321e3
Simone Castellani <sfrulloo@gmail.com>
2018-01-30 23:07:49 +0100

    add filename filter in test runner

--- tests/run_test.py
+++ tests/run_test.py
@@ -15,7 +15,14 @@ import inotify.adapters
 from inotify.constants import IN_ATTRIB, IN_CREATE, IN_CLOSE_WRITE, IN_DELETE, IN_MOVED_FROM, IN_MOVED_TO
 
 excluded_path = [".git"]
+excluded_file = [".pyc"]
 valid_mask = reduce(lambda x,y: x|y, [IN_ATTRIB, IN_CREATE, IN_CLOSE_WRITE, IN_DELETE, IN_MOVED_FROM, IN_MOVED_TO])
+
+def is_valid_path(path):
+	return not any([ p in path for p in excluded_path])
+
+def is_valid_filename(filename):
+	return not any([ filename.endswith(f) for f in excluded_file ])
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
@@ -26,7 +33,7 @@ if __name__ == '__main__':
 	test_runner = unittest.TextTestRunner(verbosity=args.verbosity)
 
 	path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
-	test_case = unittest.defaultTestLoader.discover(path)
+	test_case = unittest.TestLoader().discover(path)
 	test_runner.run(test_case)
 
 	if args.deamon:
@@ -34,6 +41,7 @@ if __name__ == '__main__':
 		for event in i.event_gen():
 			if event is not None:
 				(header, type_names, path, filename) = event
-				if not any([ p in path for p in excluded_path]):
+				if is_valid_path(path) and is_valid_filename(filename):
+					test_case = unittest.TestLoader().discover(path)
 					test_runner.run(test_case)
 
