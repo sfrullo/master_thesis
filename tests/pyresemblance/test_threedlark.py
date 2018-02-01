c7ac139447b20273d1533ad16debe416033c0762
Simone Castellani <sfrulloo@gmail.com>
2018-02-01 23:17:56 +0100

    added test for edge_mirror_3

--- tests/pyresemblance/test_threedlark.py
+++ tests/pyresemblance/test_threedlark.py
@@ -7,14 +7,25 @@ import numpy as np
 from pyresemblance import threedlark
 from pyresemblance import consts
 
-from tests.pyresemblance import consts as test_consts
+from pyresemblance.utils import edge_mirror_3
 
-class TestStringMethods(unittest.TestCase):
+from tests.pyresemblance import test_case as test_case
+
+class TestThreeDLARK(unittest.TestCase):
 
     def setUp(self):
-    	self.threeDLARK = threedlark.ThreeDLARK(w_size=3, w_size_t=3, smoothing=1.0, sensitiviy=1)
+        self.threeDLARK = threedlark.ThreeDLARK(w_size=3, w_size_t=3, smoothing=1.0, alpha=1)
 
     def test_compute_kernel(self):
-    	kernel = self.threeDLARK.compute_kernel(mask=consts.fspecial_disk_1)
-    	expected_result = test_consts.threedlark_kernel_fspecial_disk_1
+        kernel = self.threeDLARK.compute_kernel(mask=consts.fspecial_disk_1)
+        expected_result = test_case.test_threedlark_kernel_fspecial_disk_1
         np.testing.assert_allclose(kernel, expected_result, rtol=1e-7)
+
+
+class TestEdgeMirror3(unittest.TestCase):
+
+    def test_edge_mirror_3(self):
+        for tc in test_case.test_edge_mirror_3:
+            input_data, expected_result = tc.values()
+            result = edge_mirror_3(input_data, width=((1,), (1,), (1,)))
+            np.testing.assert_equal(result, expected_result)
