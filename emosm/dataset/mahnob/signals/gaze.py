2d2e96488bf4a5c34fe31ac4ca9048cdd211a76b
Simone <simone.castellani@nttdata.com>
2018-02-24 16:50:42 +0100

    add gaze data loader

--- emosm/dataset/mahnob/signals/gaze.py
+++ emosm/dataset/mahnob/signals/gaze.py
@@ -2,8 +2,29 @@
 
 import numpy as np
 
+HEADER_LENGTH = 24
+
 class GazeData(object):
     """docstring for GazeData"""
     def __init__(self, track):
         super(GazeData, self).__init__()
+        self.track = track
 
+    def load_gaze_from_file(self)
+        print 'Load gaze file.'
+
+        filename = self.track.get_filename()
+
+        with open(filename, 'r') as f:
+            # skip 24 lines
+            for i in range(HEADER_LENGTH): f.readline()
+            header = f.readfline().replace('\n', '').split('\t')
+            data = {h:[] for h in header}
+            for line in f:
+                [ data[header[i]].append(d) for i,d in enumerate(line.split('\t')) ]
+
+        self.header = header
+        self.data = data
+
+        print self.header
+        print self.data
