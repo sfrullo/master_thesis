fix saliency map normalization

emosm/sm/basesm.py
@@ -28,17 +28,10 @@ class BaseSaliencyMap(object):
 
         w, h = display_size
 
-        # heatmap = utils.grid_density_gaussian_filter(x0, y0, x1, y1, w, h, data)
-
         x, y, d = data.T
         bins = (range(w), range(h))
-        heatmap, xedges, yedges = np.histogram2d(x, y, bins=bins, weights=d, normed=True)
+        heatmap, xedges, yedges = np.histogram2d(x, y, bins=bins, weights=d, normed=normalize)
         heatmap = ndi.gaussian_filter(heatmap, sigma=8)
-
-        # extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
-
-        if normalize is True:
-            heatmap *= 1/heatmap.max()
 
         return heatmap.T
 

emosm/sm/physiosm.py
@@ -27,7 +27,7 @@ class PhysioSaliencyMap(basesm.BaseSaliencyMap):
             raise ValueError("display_size must be a tuple")
 
         # keep only fixations
-        coordinates = self.gaze["fixations"][:,:,0:2] / config.FRAME_SCALE_FACTOR
+        coordinates = self.gaze["fixations"][:,:,0:2]
         len_coor = coordinates.shape[0]
 
         # zip physiological data with proper gaze coordinate
@@ -47,8 +47,8 @@ class PhysioSaliencyMap(basesm.BaseSaliencyMap):
 
         frame_heatmap_list = np.asarray(frame_heatmap_list)
 
-        frame_heatmap_list *= 1/frame_heatmap_list.max()
-        # frame_heatmap_list = (frame_heatmap_list - frame_heatmap_list.mean())/frame_heatmap_list.max()
+        # frame_heatmap_list *= 1/frame_heatmap_list.max()
+        frame_heatmap_list = (frame_heatmap_list - frame_heatmap_list.mean())/frame_heatmap_list.max()
 
         return frame_heatmap_list
 