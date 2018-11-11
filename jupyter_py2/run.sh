8a01c7736d7cf89aa6f96c859afe057ff916b007
Simone <simone.castellani@nttdata.com>
2018-11-11 15:55:46 +0100

    added jupyter dockerfile and notebook

---
+++ jupyter_py2/run.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+PWD=`pwd`
+
+source $PWD/setenv.sh
+
+echo $EMOSM_NOTEBOOK_DIR
+echo $EMOSM_EXPORT_DIR
+
+#sudo docker run -it \
+#    -p 8888:8888 \
+#    --user root \
+#    -e CHOWN_EXTRA_OPTS='-R' \
+#    -e CHOWN_HOME=yes \
+#    -e CHOWN_EXTRA="/home/TesiCastellani/notebook,/home/TesiCastellani/export" \
+#    -e NB_USER=TesiCastellani \
+#    -w /home/TesiCastellani \
+#    -v $EMOSM_NOTEBOOK_DIR:/home/TesiCastellani/notebook \
+#    -v $EMOSM_EXPORT_DIR:/home/TesiCastellani/export \
+#    jupyter-py2:latest
+
+sudo docker run -it \
+    -p 8888:8888 \
+    -v $EMOSM_NOTEBOOK_DIR:/home/jovyan/work \
+    -v $EMOSM_EXPORT_DIR:/home/jovyan/export \
+    jupyter-py2:latest
