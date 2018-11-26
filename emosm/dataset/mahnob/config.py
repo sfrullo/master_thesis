4ef791a3d8022ecdc6f232c2fd82d5a5e46fe647
Simone <simone.castellani@nttdata.com>
2018-11-26 11:09:23 +0100

    get path from env

--- emosm/dataset/mahnob/config.py
+++ emosm/dataset/mahnob/config.py
@@ -7,26 +7,27 @@ import numpy as np
 # MAHNOB DIRECTORIES
 #
 
-DIR_BASE = '/media/sf_vbfolder/TesiCastellani/Dataset/Mahnob'
+HOME = os.environ['HOME']
+MAHNOB_DIR = os.environ['MAHNOB_DIR'] or os.path.join(HOME, 'Dataset/Mahnob')
 DIR_MAHNOB = {
     # 'MediaFiles' : os.path.join(DIR_BASE, 'data/MediaFiles'),
-    'MediaFiles' : os.path.join(DIR_BASE, 'data/media_24'),
-    'Sessions' : os.path.join(DIR_BASE, 'data/Sessions'),
-    'Subjects' : os.path.join(DIR_BASE, 'data/Subjects'),
+    'MediaFiles' : os.path.join(MAHNOB_DIR, 'data/media_24'),
+    'Sessions' : os.path.join(MAHNOB_DIR, 'data/Sessions'),
+    'Subjects' : os.path.join(MAHNOB_DIR, 'data/Subjects'),
 }
 
-EXPORT_DIR_BASE = "/media/sf_vbfolder/TesiCastellani/export"
+EXPORT_DIR_BASE = os.environ['EXPORT_DIR_BASE'] or os.path.join(HOME, "export")
 VIDEO_EXPORT_DIR_BASE = os.path.join(EXPORT_DIR_BASE, "video")
 DATA_EXPORT_DIR_BASE = os.path.join(EXPORT_DIR_BASE, "data")
 
 try:
 	os.makedirs(VIDEO_EXPORT_DIR_BASE)
-except OSError:
+except OSError as e:
 	pass
 
 try:
 	os.makedirs(DATA_EXPORT_DIR_BASE)
-except OSError:
+except OSError as e:
 	pass
 
 #
---
+++ run_demo.sh
@@ -0,0 +1,4 @@
+export MAHNOB_DIR="/home/castellanisi/vbfolder/TesiCastellani/Dataset/Mahnob"
+export EXPORT_DIR_BASE="/home/castellanisi/vbfolder/TesiCastellani/Tesi_Castellani/export"
+
+python demo.py
