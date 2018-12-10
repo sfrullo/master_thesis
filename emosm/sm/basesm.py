fix sm normalization

emosm/sm/basesm.py
@@ -23,14 +23,16 @@ class BaseSaliencyMap(object):
         if mean_data is not None:
             data = data.mean(axis=1)
 
-        x0 = y0 = 0
-        x1, y1 = display_size
+        if data.ndim == 3:
+            x, y, d = data.T
+        else:
+            x, y = data.T
+            d = np.ones(data.shape[0]).T
 
-        w, h = display_size
+        bins = display_size
 
-        x, y, d = data.T
-        bins = (range(w), range(h))
         heatmap, xedges, yedges = np.histogram2d(x, y, bins=bins, weights=d, normed=normalize)
+
         heatmap = ndi.gaussian_filter(heatmap, sigma=8)
 
         return heatmap.T

emosm/sm/gazesm.py
@@ -25,26 +25,24 @@ class GazeSaliencyMap(basesm.BaseSaliencyMap):
         if display_size is None:
             raise ValueError("display_size must be a tuple")
 
-        fixations = self.gaze_data['fixations']
+        coordinates = self.gaze_data['coordinates']
 
         if limit_frame is not None:
-            fixations = fixations[:limit_frame]
+            coordinates = coordinates[:limit_frame]
 
-        framed_sample_generator = utils.moving_window_data_per_frame_generator(fixations, ws=config.MIN_SAMPLE_WINDOW)
+        framed_sample_generator = utils.moving_window_data_per_frame_generator(coordinates, ws=config.MIN_SAMPLE_WINDOW)
 
         print "Process frame"
         frame_heatmap_list = []
         for frame_number, framed_sample in enumerate(framed_sample_generator):
-            print "# {}/{}".format(frame_number, fixations.shape[0])
+            print "# {}/{}".format(frame_number, coordinates.shape[0])
             frame_heatmap = self.compute_frame_saliency_map(framed_sample, display_size)
             # yield frame_heatmap
             frame_heatmap_list.append(frame_heatmap)
         print "End"
 
-        frame_heatmap_list = np.asarray(frame_heatmap_list)
-
-        # frame_heatmap_list *= 1/frame_heatmap_list.max()
-        frame_heatmap_list = (frame_heatmap_list - frame_heatmap_list.mean())/frame_heatmap_list.max()
+        X = np.asarray(frame_heatmap_list)
+        frame_heatmap_list = utils.normalize(X)
 
         return frame_heatmap_list
 

emosm/sm/physiosm.py
@@ -27,14 +27,21 @@ class PhysioSaliencyMap(basesm.BaseSaliencyMap):
             raise ValueError("display_size must be a tuple")
 
         # keep only fixations
-        coordinates = self.gaze["fixations"][:,:,0:2]
+        coordinates = self.gaze['coordinates']
+        physio = self.physio
         len_coor = coordinates.shape[0]
 
-        # zip physiological data with proper gaze coordinate
-        sm_data = np.concatenate([coordinates, self.physio[:len_coor,:,np.newaxis]], axis=2)
-
         if limit_frame is not None:
-            sm_data = sm_data[:limit_frame]
+            coordinates = coordinates[:limit_frame]
+            physio = physio[:limit_frame]
+
+        print coordinates.squeeze()
+        print physio
+
+        # zip physiological data with proper gaze coordinate
+        sm_data = np.append(coordinates.squeeze(), physio, axis=1)
+
+        print sm_data.shape
 
         print "Process frame"
         frame_heatmap_list = []
@@ -45,10 +52,9 @@ class PhysioSaliencyMap(basesm.BaseSaliencyMap):
             frame_heatmap_list.append(frame_heatmap)
         print "End"
 
-        frame_heatmap_list = np.asarray(frame_heatmap_list)
-
-        # frame_heatmap_list *= 1/frame_heatmap_list.max()
-        frame_heatmap_list = (frame_heatmap_list - frame_heatmap_list.mean())/frame_heatmap_list.max()
+        X = np.asarray(frame_heatmap_list)
+        X_std = (X - X.min()) / (X.max() - X.min())
+        frame_heatmap_list = X_std * (1 - 0) + 0
 
         return frame_heatmap_list
 