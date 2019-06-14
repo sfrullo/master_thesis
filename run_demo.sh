615de7332ecf160012fe9fcf91840bcd86744233
Simone Castellani <simone.castellani@nttdata.com>
2019-06-14 08:30:21 +0200

    update demo

--- run_demo.sh
+++ run_demo.sh
@@ -1,4 +1,4 @@
-export MAHNOB_DIR="/home/castellanisi/vbfolder/TesiCastellani/Dataset/Mahnob"
-export EXPORT_DIR_BASE="/home/castellanisi/vbfolder/TesiCastellani/Tesi_Castellani/export"
+export MAHNOB_DIR="/home/castellanisi/vbfolder/projects/TesiCastellani/Dataset/Mahnob"
+export EXPORT_DIR_BASE="/home/castellanisi/vbfolder/projects/TesiCastellani/Tesi_Castellani/export"
 
 python demo.py
